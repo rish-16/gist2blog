a4064a46d33e33d91e47a5323f3d4ffc67e9995e
rish-16 <mail.rishabh.anand@gmail.com>
2021-02-01 22:03:02 +0800

    Add text window for markdown rendering via GH API

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const urlTextbox = document.getElementById("gist-url-textbox")
     const submitBtn = document.getElementById("submit")
+    const textEditor = document.getElementById("md-area")
     
     const ghAPIURL = "https://api.github.com/gists/"
     
@@ -13,18 +14,28 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     submitBtn.addEventListener("click", () => {
-        var content = urlTextbox.value
-        content = getPostID(content)
-        var gistURL = ghAPIURL + content;
+        var gistID = urlTextbox.value
+        gistID = getPostID(gistID)
+        var gistURL = ghAPIURL + gistID;
         
-        const Http = new XMLHttpRequest();
-
-        Http.open("GET", gistURL);
-        Http.send();
-
-        Http.onreadystatechange = (e) => {
-            console.log(e)
-            console.log(Http.responseText)
-        }                
+        // https://gist.github.com/rish-16/959865cac059c9a46881cdc95a24bfc0
+        
+        var req = new XMLHttpRequest();
+        req.responseType = 'json';
+        req.open('GET', gistURL, true);
+        
+        req.onload  = function() {
+            var jsonResponse = req.response;
+            var gistContent = jsonResponse["files"]
+            var filename = Object.keys(gistContent)[0]
+            
+            var markdownContent = jsonResponse["files"][filename]["content"]
+            
+            var converter = new showdown.Converter()
+            html = converter.makeHtml(markdownContent)
+            textEditor.style.fontFamily = "Avenir Next"
+            textEditor.innerHTML = html
+        };
+        req.send(null);        
     })
 })
